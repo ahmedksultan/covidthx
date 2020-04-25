b3322e2db3e2a7743e7c85a73504968dbaa960c4
vsofat <vsofat00@stuy.edu>
2020-04-24 22:20:14 -0400

    fixed

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-
-app.set('view engine', 'pug')
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey'})
-})
+)
 
 });
 
