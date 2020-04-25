5ae711dae5e1a15b1a12a9b72b39fabd43065b11
vsofat <vsofat00@stuy.edu>
2020-04-25 02:29:19 -0400

    change in templating

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var nunjucks  = require('nunjucks');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +25,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function(req, res){
+    res.render('index.html', {title: 'Wall of Thanks'});
+});
+
+app.get('/users', function(req, res) {
+    res.render('users.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
