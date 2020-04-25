7ec2a02a97b2d3f30ffe5959af82924067cefdac
vsofat <vsofat00@stuy.edu>
2020-04-24 22:17:31 -0400

    / works but doesnt load index idk

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 
+app.set('view engine', 'pug')
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey'})
+})
+
 });
 
 module.exports = router;
