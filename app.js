728c1b5a46604cb43311de70206064f2816cd1a5
Kazi Jamal <jamalkazi02@gmail.com>
2020-04-26 14:55:38 -0400

    Working errors and css

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use("/css", express.static(__dirname + "/css"));
 
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error.html');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
