f8c3e3cf89e24a273b6a5f9e6e83c66f6b406135
Kazi Jamal <jamalkazi02@gmail.com>
2020-05-03 01:51:42 -0400

    Add mongoose configuration

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
+require("dotenv").config();
 const express = require("express");
 const nunjucks = require("nunjucks");
 const app = express();
 const unirest = require("unirest");
+const mongoose = require("mongoose");
 
+// Configure Nunjucks
 nunjucks.configure("views", {
     autoescape: true,
     express: app,
 });
 
+// Configure Mongoose
+const db = process.env.MONGO_URI;
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+
+// Routes
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use("/css", express.static(__dirname + "/css"));
-app.use('/public', express.static(__dirname + "/public"));
-app.get('/geo', (req, res) => {
-  const apiCall = unirest(
-   "GET",
-   "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
- );
- apiCall.headers({
-   "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
-   "x-rapidapi-key": "7b44ded841msha82bd7cd066097fp1c9fdajsna23bb161b783"
- });
- 
- apiCall.end(function(result) {
-   if (res.error) throw new Error(result.error);
-   console.log(result.body);
-   res.send(result.body);
- });
-})
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/geo", (req, res) => {
+    const apiCall = unirest(
+        "GET",
+        "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+    );
+    apiCall.headers({
+        "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+        "x-rapidapi-key": "7b44ded841msha82bd7cd066097fp1c9fdajsna23bb161b783",
+    });
 
-app.use(function(req, res, next) {
-  res.status(404).send("Sorry can't find that!")
+    apiCall.end(function (result) {
+        if (res.error) throw new Error(result.error);
+        console.log(result.body);
+        res.send(result.body);
+    });
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!");
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error.html');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error.html");
 });
-
 
 const PORT = process.env.PORT || 5000;
 
