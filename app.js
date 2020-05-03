58b3d818684f00fe0f9b244fbfdf97168bec9266
vsofat <vsofat00@stuy.edu>
2020-05-02 20:49:59 -0400

    set up api in /geo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
-
 const app = express();
+const unirest = require("unirest");
 
 nunjucks.configure("views", {
     autoescape: true,
@@ -12,6 +12,22 @@ app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use("/css", express.static(__dirname + "/css"));
 app.use('/public', express.static(__dirname + "/public"));
+app.get('/geo', (req, res) => {
+  const apiCall = unirest(
+   "GET",
+   "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+ );
+ apiCall.headers({
+   "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+   "x-rapidapi-key": "7b44ded841msha82bd7cd066097fp1c9fdajsna23bb161b783"
+ });
+ 
+ apiCall.end(function(result) {
+   if (res.error) throw new Error(result.error);
+   console.log(result.body);
+   res.send(result.body);
+ });
+})
 
 app.use(function(req, res, next) {
   res.status(404).send("Sorry can't find that!")
@@ -27,7 +43,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error.html');
 });
-
 
 
 const PORT = process.env.PORT || 5000;
