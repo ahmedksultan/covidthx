1f95affd736c4d84898e259a3947e16166844396
Kazi Jamal <jamalkazi02@gmail.com>
2020-05-07 00:56:52 -0400

    Functioning pagination

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,28 @@ var router = express.Router();
 const Card = require("../models/Card");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-    Card.find({})
-        .sort({ timestamp: 'desc' })
-        .exec((err, cards) => {
-            array = cards;
-            res.render("index.html", { title: "Wall of Thanks", posts: array });
+router.get("/", async function (req, res, next) {
+    var perPage = 50;
+    var queryPage = req.query["page"];
+    if (queryPage == undefined) {
+        queryPage = 0;
+    }
+    var page = Math.max(0, queryPage);
+
+    Card.find()
+        .limit(perPage)
+        .skip(perPage * page)
+        .sort({
+            timestamp: "desc",
+        })
+        .exec(function (err, cards) {
+            Card.countDocuments().exec(function (err, count) {
+                res.render("index.html", {
+                    posts: cards,
+                    page: page,
+                    pages: Math.ceil(count / perPage),
+                });
+            });
         });
 });
 
