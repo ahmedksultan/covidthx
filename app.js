86025f9aee923cc0fc28419f27b7ab97b615c431
Kazi Jamal <jamalkazi02@gmail.com>
2020-05-08 23:27:41 -0400

    Cors part 2

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ const passport = require("passport");
 const flash = require("connect-flash");
 
 const app = express();
-app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 // Configure Body-Parser
 app.use(bodyParser.json());
