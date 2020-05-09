13e888f4cab114fd8c51bcd11ca0e5b4a33522fc
Kazi Jamal <jamalkazi02@gmail.com>
2020-05-09 00:08:22 -0400

    Fix cors

--- app.js
+++ app.js
@@ -12,15 +12,6 @@ const app = express();
 var cors = require("cors");
 app.use(cors());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
-
 // Configure Body-Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
